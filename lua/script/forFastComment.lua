1a416da6c22c6e4e03f3e608b4d881a3cf377814
Chris Hung <yinye1986@gmail.com>
2024-09-23 22:13:04 +0800

    bak

--- lua/script/forFastComment.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "cl", "<C-[>-- <ESC>")
--- plugin/entry.lua
+++ plugin/entry.lua
@@ -5,12 +5,12 @@ require("script.forCenteringCursor")
 require("script.forSwitchFcitx5")
 
 require("script.forFlipVal")
-vim.keymap.set("n", "'f", ':lua vim.cmd(_G.Flip)<CR>', {noremap=true, silent=true})
+vim.keymap.set("n", "'f", ':lua vim.cmd(_G.Flip)<CR><ESC>', { noremap = true, silent = true })
 
 require("script.forFastComment")
 
 
-vim.cmd[[
+vim.cmd [[
      autocmd BufRead,BufNewFile *.md luafile ~/.local/share/nvim/lazy/nvimScript/lua/ftplugin/markdown/previewer.lua
      autocmd BufRead,BufNewFile *.md luafile ~/.local/share/nvim/lazy/nvimScript/lua/ftplugin/markdown/snippet.lua
      autocmd BufRead,BufNewFile *.md nnoremap sp :lua vim.cmd(_G.StartPv)<CR>
